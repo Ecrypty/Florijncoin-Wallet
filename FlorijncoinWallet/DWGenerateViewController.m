39f1079a874cfaf39c1d162a434fe4cdc2456687
Ecrypty Development <Ecrypty@macbook-pro-van-ecrypty.local>
2018-11-05 23:36:55 +0100

    Major update 1 (languages, backgrounds, basics)

---
+++ FlorijncoinWallet/DWGenerateViewController.m
@@ -0,0 +1,76 @@
+//
+//  DWGenerateViewController.m
+//  dashwallet
+//
+//  Created by Quantum Explorer on 10/11/17.
+//  Copyright © 2018 Ecrypty Group | Florijncoin. All rights reserved.
+//
+
+#import "DWGenerateViewController.h"
+#import "BREventManager.h"
+#import "BRWalletManager.h"
+#import "DWWarningViewController.h"
+
+@interface DWGenerateViewController ()
+
+@property (nonatomic, strong) IBOutlet UIButton *generateButton, *showButton;
+@property (nonatomic, strong) IBOutlet UILabel *startLabel, *recoverLabel;
+@property (nonatomic, strong) UINavigationController *seedNav;
+
+@end
+
+@implementation DWGenerateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.generateButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    self.generateButton.titleLabel.adjustsLetterSpacingToFitWidth = YES;
+#pragma clang diagnostic pop
+    
+    //self.generateButton.superview.backgroundColor = [UIColor clearColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)prefersStatusBarHidden {
+    return FALSE;
+}
+
+
+-(UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+-(BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    if (![BRWalletManager sharedInstance].passcodeEnabled) {
+        [BREventManager saveEvent:@"welcome:passcode_disabled"];
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:NSLocalizedString(@"turn device passcode on", nil)
+                                     message:NSLocalizedString(@"\nA device passcode is needed to safeguard your wallet. Go to settings and turn "
+                                                               "passcode on to continue.", nil)
+                                     preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* okButton = [UIAlertAction
+                                   actionWithTitle:NSLocalizedString(@"ok", nil)
+                                   style:UIAlertActionStyleCancel
+                                   handler:^(UIAlertAction * action) {
+                                   }];
+        [alert addAction:okButton];
+        [self presentViewController:alert animated:YES completion:nil];
+        return FALSE;
+    }
+    return TRUE;
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    [BREventManager saveEvent:@"welcome:generate"];
+}
+
+@end
